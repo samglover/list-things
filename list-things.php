4282306baf72003cc243cfb31514d1787ee81850
samglover <sam@samglover.net>
2025-02-02 11:34:59 -0600

    Reformat with WP coding standards

--- list-things.php
+++ list-things.php
@@ -10,14 +10,16 @@
 
 namespace List_Things;
 
-if (!defined('ABSPATH')) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Constants
  */
-define('LIST_THINGS_VERSION', '0.2');
-define('LIST_THINGS_DIR_PATH', plugin_dir_path(__FILE__));
-define('LIST_THINGS_DIR_URL', plugin_dir_url(__FILE__));
+define( 'LIST_THINGS_VERSION', '0.2' );
+define( 'LIST_THINGS_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'LIST_THINGS_DIR_URL', plugin_dir_url( __FILE__ ) );
 
 /**
  * Plugin Files
@@ -25,13 +27,13 @@ define('LIST_THINGS_DIR_URL', plugin_dir_url(__FILE__));
 
 // Common
 // require_once(LIST_THINGS_DIR_PATH . 'common/search-things.php');
-require_once(LIST_THINGS_DIR_PATH . 'common/things.php');
-require_once(LIST_THINGS_DIR_PATH . 'common/ajax-things.php');
-require_once(LIST_THINGS_DIR_PATH . 'common/utilities.php');
+require_once LIST_THINGS_DIR_PATH . 'common/things.php';
+require_once LIST_THINGS_DIR_PATH . 'common/ajax-things.php';
+require_once LIST_THINGS_DIR_PATH . 'common/utilities.php';
 
 // Frontend
-if (!is_admin()) {
-  require_once(LIST_THINGS_DIR_PATH . 'frontend/form-search-things.php');
-  require_once(LIST_THINGS_DIR_PATH . 'frontend/form-sort-things.php');
-  require_once(LIST_THINGS_DIR_PATH . 'frontend/shortcode.php');
+if ( ! is_admin() ) {
+	require_once LIST_THINGS_DIR_PATH . 'frontend/form-search-things.php';
+	require_once LIST_THINGS_DIR_PATH . 'frontend/form-sort-things.php';
+	require_once LIST_THINGS_DIR_PATH . 'frontend/shortcode.php';
 }
