2e6932a47beba7f433ec93723293de16738e7fb5
samglover <sam@samglover.net>
2025-12-07 23:49:18 -0600

    Create form-filter-things.php

---
+++ frontend/form-filter-things.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Handles filtering things.
+ *
+ * @file       form-filter-things.php
+ * @package    list-things
+ * @subpackage frontend
+ * @since      0.4.0
+ */
+
+namespace List_Things;
