4bce006f644ce301ab52c886bf0661f1db9c1abf
samglover <sam@samglover.net>
2025-12-08 00:08:55 -0600

    Add frontend/form-filter-things.php

--- frontend/form-filter-things.php
+++ frontend/form-filter-things.php
@@ -9,3 +9,25 @@
  */
 
 namespace List_Things;
+
+/**
+ * Outputs the thing-filterers.
+ *
+ * @param array $args    Optional. Array of query arguments.
+ * @param array $options Optional. Array of display options.
+ */
+function filter_things( $args, $options ) {
+	$post_type_name = format_list_of_things( get_post_type_names( $args['post_type'] ), 'and' );
+	?>
+	<div 
+		id="thing-filters-<?php echo esc_attr( $options['things_section_id'] ); ?>" 
+		class="thing-filter" 
+		role="search" 
+		onsubmit="return false;"
+	>
+		<button class="wp-element-button has-sm-font-size button things-filters-button" type="button">
+			<?php esc_html_e( 'Show filters', 'list-things' ); ?>
+		</button>
+	</div>
+	<?php
+}
